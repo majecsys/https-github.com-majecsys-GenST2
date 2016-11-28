0f9de670d2fd23399e9d0e996db3a73c3e1ec751
eguinn <eguinn@majec.com>
2016-11-28 12:45:58 -0600

    first commit

--- GenST2/Checkin.aspx.cs
+++ GenST2/Checkin.aspx.cs
@@ -11,7 +11,6 @@ namespace GenST2
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
         }
         public IQueryable<Models.checkin> lvCheckIn_GetData()
         {
